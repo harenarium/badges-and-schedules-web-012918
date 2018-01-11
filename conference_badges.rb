d134637a6f09e8b81c76fed48eb7aa1598dac916
Caroline Lee <carolinel2610@gmail.com>
2018-01-11 23:52:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms method(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms method(attendees).each do |room|
+    puts room
+  end
 end
